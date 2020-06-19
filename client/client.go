7adad5464320ad7f35d3d208b642910f3c410638
Qi Binyi <qibinyi@Qis-MacMini2018.local>
2020-06-19 12:29:17 +0800

    increase http client MaxConnsPerHost to 128

--- client/client.go
+++ client/client.go
@@ -1,22 +1,22 @@
 package client
 
 import (
-    "log"
+	"log"
 	// "crypto/sha256"
 	"crypto/tls"
+	"errors"
+	"io"
+	"io/ioutil"
 	"net/http"
-    "net/url"
+	"net/url"
 	"os"
-    "time"
-    "io/ioutil"
-    "errors"
-    "io"
+	"time"
 )
 
 var http_client *http.Client
 
 func init() {
-    var proxyURL *url.URL = nil
+	var proxyURL *url.URL = nil
 	http_proxy := os.Getenv("HTTP_PROXY")
 	if http_proxy != "" {
 		p, err := url.Parse(http_proxy)
@@ -27,55 +27,55 @@ func init() {
 			log.Println("proxyURL ", proxyURL)
 		}
 	}
-    tr := &http.Transport{
-        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-    }
-    if proxyURL != nil {
-        tr.Proxy = http.ProxyURL(proxyURL)
-    }
-    http_client = &http.Client{Timeout: 15 * time.Second, Transport: tr}
-}
 
+	nConcurrency := 128
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		MaxConnsPerHost: nConcurrency,
+		//MaxIdleConns:        nConcurrency,
+		//MaxIdleConnsPerHost: nConcurrency,
+	}
+	if proxyURL != nil {
+		tr.Proxy = http.ProxyURL(proxyURL)
+	}
+	http_client = &http.Client{Timeout: 15 * time.Second, Transport: tr}
+}
 
 func Client() *http.Client {
-    return http_client 
+	return http_client
 }
 
+func readResponse(res *http.Response, err error) (string, error) {
 
-func readResponse( res *http.Response , err error  )  ( string, error  ) {
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
 
-    if err != nil {
-        log.Println(err)
-        return "", err
-    }
+	b, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
 
-    b, err := ioutil.ReadAll(res.Body)
-    res.Body.Close()
-    if err != nil {
-        log.Println(err)
-        return "", err
-    }
+	str_body := string(b)
 
-    str_body := string(b)
+	if res.StatusCode >= 200 && res.StatusCode <= 299 {
+	} else {
+		return "", errors.New(str_body)
+	}
 
-    if res.StatusCode >= 200 && res.StatusCode <= 299 {
-    } else {
-        return ""  ,  errors.New( str_body )  
-    }
-    
-    return str_body , nil 
+	return str_body, nil
 
 }
 
-func Get( url string ) ( string , error  ) {
-    res, err := http_client.Get(url)
-    return readResponse( res, err )
+func Get(url string) (string, error) {
+	res, err := http_client.Get(url)
+	return readResponse(res, err)
 }
 
-
-
-func Post( url string , contentType string  ,data io.Reader ) ( string , error  ) {
-    res, err := http_client.Post( url, contentType  , data  )
-    return readResponse( res, err )
+func Post(url string, contentType string, data io.Reader) (string, error) {
+	res, err := http_client.Post(url, contentType, data)
+	return readResponse(res, err)
 }
-
